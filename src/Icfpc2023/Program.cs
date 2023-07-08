88aaf8a7cf09e8d65d229ab596c9d35797b10d9f
Leonid Olevskiy <leonid.olevskiy@aliexpress.ru>
2023-07-08 13:20:35 +0300

    Update scoring according API

--- src/Icfpc2023/Program.cs
+++ src/Icfpc2023/Program.cs
@@ -12,75 +12,78 @@ internal static class Program
         var apiToken = Environment.GetEnvironmentVariable("API_TOKEN");
         using var apiClient = new ApiClient(apiToken);
         var problems = await apiClient.GetProblemsDefinition();
-        var problemId = 22;
+        var problemId = 1;
 
-        foreach (var problem in problems.Skip(problemId - 1))
+        await Parallel.ForEachAsync(problems.Zip(Enumerable.Range(1, int.MaxValue)),
+            async (p, _) => await ProcessProblem(p.First, apiClient, p.Second));
+    }
+
+    private static async Task ProcessProblem(Problem problem, ApiClient apiClient, int problemId)
+    {
+        Console.WriteLine($"[{problemId}] Start processing");
+
+        var numberOfInstruments = problem.Musicians.Max() + 1;
+
+        var instruments = Enumerable.Range(0, (int)numberOfInstruments)
+            .Select(i => new Instrument((uint)i))
+            .ToDictionary(i => i.Id);
+
+        var musicians = problem.Musicians
+            .Select((instrument, i) => new Musician(i, instruments[instrument]))
+            .ToArray();
+
+        var scene = new Scene
         {
-            var numberOfInstruments = problem.Musicians.Max() + 1;
+            Height = problem.StageHeight,
+            Width = problem.StageWidth,
+            BottomLeft = new PointDto
+            (
+                problem.StageBottomLeft.First(),
+                problem.StageBottomLeft.Last()
+            )
+        };
 
-            var instruments = Enumerable.Range(0, (int)numberOfInstruments)
-                .Select(i => new Instrument((uint)i))
-                .ToDictionary(i => i.Id);
+        var xMiddle = scene.BottomLeft.X + scene.Width / 2;
+        var yMiddle = scene.BottomLeft.Y + scene.Height / 2;
 
-            var musicians = problem.Musicians
-                .Select((instrument, i) => new Musician(i, instruments[instrument]))
-                .ToArray();
+        foreach (var musician in musicians)
+        {
+            musician.AdjustPosition(new PointDto
+            (
+                xMiddle,
+                yMiddle
+            ));
+        }
 
-            var scene = new Scene
-            {
-                Height = problem.StageHeight,
-                Width = problem.StageWidth,
-                BottomLeft = new PointDto
-                (
-                    problem.StageBottomLeft.First(),
-                    problem.StageBottomLeft.Last()
-                )
-            };
-
-            var xMiddle = scene.BottomLeft.X + scene.Width / 2;
-            var yMiddle = scene.BottomLeft.Y + scene.Height / 2;
-
-
-            foreach (var musician in musicians)
-            {
-                musician.AdjustPosition(new PointDto
-                (
-                    xMiddle,
-                    yMiddle
-                ));
-            }
-
-            var listeners = problem.Attendees
-                .Select((a, i) => new Listener(
-                    i,
-                    new PointDto(a.X, a.Y),
-                    a.Tastes.Select((t, j) => (t, j)).ToDictionary(
-                        kv => instruments[(uint)kv.j],
-                        kv => new Taste
-                        {
-                            Instrument = instruments[(uint)kv.j],
-                            Value = kv.t
-                        }))).ToArray();
-
-            var calculator = new ScoreCalculator();
-
-            var solver = new Solver(10f, 100);
-            var score = solver.Solve(calculator, scene, listeners, musicians);
-            Console.WriteLine($"Resulting score is {score}");
-
-            if (score > 0)
-            {
-                await apiClient.Submit((uint)problemId, new Placements
-                {
-                    PlacementsList = musicians.Select(m => new Coords
+        var listeners = problem.Attendees
+            .Select((a, i) => new Listener(
+                i,
+                new PointDto(a.X, a.Y),
+                a.Tastes.Select((t, j) => (t, j)).ToDictionary(
+                    kv => instruments[(uint)kv.j],
+                    kv => new Taste
                     {
-                        X = m.Position.X,
-                        Y = m.Position.Y
-                    }).ToList()
-                });
-            }
+                        Instrument = instruments[(uint)kv.j],
+                        Value = kv.t
+                    }))).ToArray();
 
-            problemId++;
+        var calculator = new ScoreCalculator();
+
+        var solver = new Solver(10f, 100);
+        var score = solver.Solve(calculator, scene, listeners, musicians);
+
+        Console.WriteLine($"[{problemId}] Resulting score is {score}");
+
+        if (score > 0)
+        {
+            await apiClient.Submit((uint)problemId, new Placements
+            {
+                PlacementsList = musicians.Select(m => new Coords
+                {
+                    X = m.Position.X,
+                    Y = m.Position.Y
+                }).ToList()
+            });
         }
     }
 }
